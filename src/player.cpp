c0382f74801707bbeaa1570b15144fdb4cc281f2
David Mitsche <david.mitsche@student.tugraz.at>
2025-04-18 03:01:09 +0200

    added an asset for the player model but the animation is still fucked

--- src/player.cpp
+++ src/player.cpp
@@ -1,11 +1,27 @@
 #include "player.hpp"
 
-Player::Player(Window &window) : renderer_(window.getRenderer())
+Player::Player(Window &window) 
+  : renderer_(window.getRenderer()),
+    velocity_x_(0),
+    velocity_y_(0),
+    gravity_(1),
+    jump_strength_(-20),
+    is_jumping_(false),
+    frame_width_(64),
+    frame_height_(64),
+    current_frame_(0),
+    frame_count_(6),
+    animation_timer_(0),
+    animation_speed_(10)
 {
-  player_rect_.h = 200;
-  player_rect_.w = 50;
+  player_rect_.h = frame_width_ * 2;
+  player_rect_.w = frame_height_ * 2;
   player_rect_.x = 700;
   player_rect_.y = window.getScreenHeight() - player_rect_.h - (window.getScreenHeight() / 6);
+
+  SDL_Surface *surface = IMG_Load("assets/WarriorMan-Sheet.png");
+  sprite_sheet_ = SDL_CreateTextureFromSurface(renderer_, surface);
+  SDL_FreeSurface(surface);
 }
 
 void Player::handleInput(const SDL_Event &event)
@@ -52,11 +68,28 @@ void Player::update()
     velocity_y_ = 0;
     is_jumping_ = false;
   }
+
+  animation_timer_++;
+  if (animation_timer_ >= animation_speed_)
+  {
+    current_frame_ = (current_frame_ + 1) % frame_count_;
+    animation_timer_ = 0;
+  }
 }
 
 void Player::render()
 {
-  SDL_SetRenderDrawColor(renderer_, 255, 0, 0, 255);
-  SDL_RenderFillRect(renderer_, &player_rect_);
-  SDL_RenderDrawRect(renderer_, &player_rect_);
+  SDL_Rect src_rect;
+  src_rect.x = current_frame_ * frame_width_;
+  src_rect.y = 0;
+  src_rect.w = frame_width_;
+  src_rect.h = frame_height_;
+
+  SDL_Rect dest_rect;
+  dest_rect.x = player_rect_.x;
+  dest_rect.y = player_rect_.y;
+  dest_rect.w = player_rect_.w;
+  dest_rect.h = player_rect_.h;
+
+  SDL_RenderCopy(renderer_, sprite_sheet_, &src_rect, &dest_rect);
 }
--- src/player.hpp
+++ src/player.hpp
@@ -5,6 +5,7 @@
 #include "game.hpp"
 #include "window.hpp"
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 
 class Player
 {
@@ -16,6 +17,14 @@ class Player
     int gravity_ = 1;
     int jump_strength_ = -20;
     bool is_jumping_ = false;
+
+    int frame_width_;
+    int frame_height_;
+    int current_frame_;
+    int frame_count_;
+    int animation_timer_;
+    int animation_speed_;
+    SDL_Texture *sprite_sheet_;
 
   public:
     Player(Window &window);
